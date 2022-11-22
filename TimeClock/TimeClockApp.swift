a1315ced2c374ff5731729467080ff6edc873a91
Kyle Peterson <kdpeters@gmail.com>
2022-11-22 11:55:36 -0500

    Initial Commit

---
+++ TimeClock/TimeClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeClockApp.swift
+//  TimeClock
+//
+//  Created by Kyle Peterson on 11/22/22.
+//
+
+import SwiftUI
+
+@main
+struct TimeClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
